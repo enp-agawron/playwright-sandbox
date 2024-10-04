435382860d89c7f5c7594d618c45460a747f79b5
Artur Gawron <artur@gawron.me>
2024-10-04 23:42:00 +0200

    feat(test): add list and dropdowns

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -96,6 +96,23 @@ test('LIST AND DROPDOWNS', async ({ page }) => {
         'Light',
         'Dark',
         'Cosmic',
-        'Corporated'
+        'Corporate'
     ])
+    await optionList.filter({ hasText: 'Cosmic' }).click()
+    const header = page.locator('nb-layout-header')
+    await expect(header).toHaveCSS('background-color', 'rgb(50, 50, 89)')
+
+    const colors = {
+        Light: 'rgb(255, 255, 255)',
+        Dark: 'rgb(34, 43, 69)',
+        Cosmic: 'rgb(50, 50, 89)',
+        Corporate: 'rgb(255, 255, 255)'
+    }
+
+    await dropDownMenu.click()
+    for (const color in colors) {
+        await optionList.filter({ hasText: color }).click()
+        await expect(header).toHaveCSS('background-color', colors[color])
+        if (color !== 'Corporate') await dropDownMenu.click()
+    }
 })
