7807ab3463b62a9b21dfbf0e5eadc8778d776822
Artur Gawron <artur@gawron.me>
2024-10-12 00:25:24 +0200

    fix(test): fix cookies modal apear

--- tests/dragAndDropIFrame.spec.ts
+++ tests/dragAndDropIFrame.spec.ts
@@ -1,7 +1,8 @@
 import { test, expect } from '@playwright/test'
 
 test('DRAG AND DROP WITH IFRAME', async ({ page }) => {
-    await page.goto('https://www.globalsqa.com/demo-site/draganddrop/')
+    await page.goto('https://www.globalsqa.com/demo-site/draganddrop')
+    page.getByLabel('Consent', { exact: true }).click() //accepted cookies
 
     const frame = page.frameLocator('[rel-title="Photo Manager"] iframe')
     await frame.locator('li', { hasText: 'High Tatras 2' }).dragTo(frame.locator('#trash'))
